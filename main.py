f81c29c91bdda39a31351e54ede1712442f1696b
Максим Йорж <iorhmaks@gmail.com>
2024-04-28 20:48:18 +0300

    update 3

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
-import functions
+
 from PIL import ImageTk, Image
 
+listKoef = []
+listViln = []
 root = tk.Tk()
 root.title("Головне вікно")
 
@@ -20,8 +22,40 @@ def option():
 
 
 def matrixKoef():
+    def zchitKoef():
+        listKoef = []
+        for koef in ent31.get().split(','):
+            listKoef.append(int(koef))
+
+    def zchitViln():
+        listViln = []
+        for viln in ent32.get().split(','):
+            listViln.append(int(viln))
+
     root3 = tk.Toplevel(root)
-    root3.mainloop()
+    lab31 = tk.Label(root3, text='Введіть коефіціенти СЛАР:', font=("Times New Roman", 15))
+    ent31 = tk.Entry(root3)
+    but31 = tk.Button(root3, text='Зчитати коефіціенти матриці', font=("Times New Roman", 15), command=zchitKoef)
+    lab32 = tk.Label(root3, text='Введіть вектор вільних коефіціентів:', font=("Times New Roman", 15))
+    ent32 = tk.Entry(root3)
+    but32 = tk.Button(root3, text='Зчитати вектор вільних коефіціентів', font=("Times New Roman", 15),
+                      command=zchitViln)
+    ent31.grid(row=1, column=0, sticky='wens')
+    lab31.grid(row=0, column=0, sticky='wens')
+    but31.grid(row=2, column=0, sticky='wens')
+    ent32.grid(row=4, column=0, sticky='wens')
+    lab32.grid(row=3, column=0, sticky='wens')
+    but32.grid(row=5, column=0, sticky='wens')
+    root3.grid_rowconfigure(0, minsize=50)
+    root3.grid_rowconfigure(1, minsize=30)
+    root3.grid_rowconfigure(2, minsize=50)
+    root3.grid_rowconfigure(3, minsize=50)
+    root3.grid_rowconfigure(4, minsize=30)
+    root3.grid_rowconfigure(5, minsize=50)
+    root3.grid_columnconfigure(0, minsize=300)
+    root3.grid_columnconfigure(1, minsize=300)
+    root3.geometry("345x270")
+    root3.wait_window()
 
 
 # labels
